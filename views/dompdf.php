8b35f62617e0857122a2bb39e0201211a252c06a
ISMG30 <m30g05@gmail.com>
2023-03-25 13:18:09 -0600

    Modificacion de la Grafica

--- views/dompdf.php
+++ views/dompdf.php
@@ -63,7 +63,7 @@ $html= ob_start();
         $fechafi1=$_GET['endDate'];
         $query =$this -> cn -> query("SELECT TC.nombre as combustible, E.litros, E.totalCosto as costo, round((E.totalCosto / E.litros),2) as costol, E.fecha FROM entradas_combustible as E INNER JOIN tipo_combustible as TC ON E.tipo_combustible = TC.id_tipo_com WHERE E.id_unidad = $id 
         AND E.fecha BETWEEN   '$fechain2'  AND '$fechafi1' ORDER BY E.fecha ASC");
-        $query2 = $this -> cn -> query("SELECT U.id_unidad AS id, U.nombre AS unidad, C.litros, C.fecha as fecha_combustible, C.tipo_check as check_combustible FROM unidad as U INNER JOIN combustible AS C ON U.id_unidad = C.id_unidad WHERE U.id_unidad = $id AND C.fecha BETWEEN ' $fechain2' AND ' $fechafi1'");
+    
         while ($con = $query -> fetch(PDO:: FETCH_NUM))
         {
             $arrayre [] = array(
@@ -74,34 +74,19 @@ $html= ob_start();
                 'fecha' => $con[4],
             );
         }
-        for($i=0; $i<count($arrayre); $i++)
+        /*$query = $this -> cn -> query("SELECT C.id_unidad, C.litros ,C.fecha, round((C.litros + EC.litros),2) AS costol FROM  combustible AS C INNER JOIN  entradas_combustible AS EC WHERE C.id_unidad= $id AND C.tipo_check = 'inicio' AND C.fecha BETWEEN $fechain2 AND $fechafi1 AND EC.fecha BETWEEN $fechain2 AND $fechafi1  AND EC.fecha = C.fecha AND EC.id_unidad = $id");
+        while ($con = $query -> fetch(PDO::FETCH_NUM))
         {
-            $fechar = $arrayre[$i]['fecha'];
-            $litrosr = $arrayre[$i]['litros'];
-        }
-        while ( $cons = $query2 -> fetch(PDO::FETCH_NUM) )
-        {
-             $arraycom [] = array(
-                'litros' => $cons[2],
-                'fecha' => $cons[3],
-                'check' => $cons[4]
-             );
-        }
-        for($i=0; $i<count($arraycom); $i++)
-        {
-            $fecha = $arraycom[$i]['fecha'];
-            $litros = $arraycom[$i]['litros'];
-            $check = $arraycom[$i]['check'];
-        }
-        if($fecha == $fechar)
-        {
-            if($check == 'inicio')
-            {
-                $result = $litros + $litrosr;
-                $arrayrest [] = array("resul"=>$result);
-            }
-        }
-       //return $arrayrest;
+            $arrayres [] = array(
+                
+                'litros' => $con[1],
+                'fecha' => $con[2],
+                //'costo' => $con[2],
+                'costol' => $con[3],
+                
+            );
+        }*/
+        return $arrayre;
     }
     function costor()
     {
@@ -487,13 +472,14 @@ $html= ob_start();
                 <div>
                     <h3 align="center"> <label class="form-label fw-bold"> Grafica</label></h3>
                     <?php         
+                        $recarga2 = $request->recargagas2();
+                        echo json_encode($recarga2);
 
                         for($i=0; $i<count($recar); $i++)
                         {
                             $litror = $recar[$i]['l'];
                             $fechaR = $recar[$i]['date'];
                             $arrayre [] = array("LitroRe" => $litror, "fechaR" => $fechaR);
-                            
                         }            
                         $recarga = json_encode($arrayre);
                         for($i=0; $i<count($jsondco);$i++)
@@ -501,50 +487,48 @@ $html= ob_start();
                             $litro = $jsondco[$i]['litros'];
                             $fechac = $jsondco[$i]['fecha'];
                             $check = $jsondco[$i]['check'];
-                            $arrayl []= array("litro"=>$litro);
+                            $arrayl []= array("litro"  => $litro);
                             $arraye [] = array("litro" => $litro, "fechac" => $fechac, "check" => $check);
                         }
                         $arraycol = array_column($arrayl, 'litro');
                          $aej = json_encode($arraye);
-                        for($j=0; $j<count($arraye); $j++)
-                        { 
-                            if($arraye[$j]["check"] === 'inicio')
-                            {  
-                                for($i=0; $i<count($arrayre); $i++)
-                                { 
-                                     if($arrayre[$i]["fechaR"] == $arraye[$j]["fechac"])
-                                    {
-                                        $suma = $arrayre[$i]["LitroRe"]+ $arraye[$j]["litro"];    
-                                        $array [] = array('litros' => $suma);
-                                    }else{
-                                        $suma = $arraye[$j]["litro"];
-                                        $array [] = array('litros' => $suma);
-                                    }
-                                } 
-                            } 
-                            if($arraye[$j]["check"] == "fin")
-                             {
-                                    $suma = $arraye[$j]["litro"];
-                                    $array [] = array('litros' =>$suma);   
-                                    
-                                }
-                        }
-                       // echo json_decode($array);
+                      
+                         for($i =0; $i<count($jsondco);$i++)
+                         {
+                            $litroc = $jsondco[$i]['litros'];
+                            $fechac = $jsondco[$i]['fecha'];
+                            $arrayf [] = array("afecha" => $fechac);
+                            $checkc = $jsondco[0]['check'];
+                           
+                         }
+                         for($i=0; $i<count($arrayf); $i++)
+                         {
+                             $row = $arrayf[0]['afecha'];
+                            // echo json_encode($row);
+                           if($arrayf[$i]['afecha'] === '2023-03-06')
+                           {
+                                 //cho json_encode($arrayf[$i]['afecha']);
+                           }else{
+                            // echo json_encode($arrayf[$i]['afecha']);
+                           }
+
+                         }
+                         //echo json_encode($arrayf );
+
                        //echo implode(",", $arraycol );
                         for($j=0; $j<count($arraye); $j++)
                         {
                             if($arraye[$j]["check"] === "inicio")
                             {
-                                if($arraye[$j]["fechac"] === "2023-03-06")
+                                if($arraye[$j]["fechac"] ===  $fechac)
                                 {
-                                    $sum = $arraye[$j]["litro"] + '80';
+                                    $sum = $arraye[$j]["litro"] + $litro;
                                     $array [] = array('litros' => $sum);
                                     //echo json_encode( $array);
                                 }else{
                                     
                                    $sum = $arraye[$j]["litro"];
                                    $array [] = array('litros' =>$sum);
-                                
                                 }
                             }
                             if($arraye[$j]["check"] === "fin")
@@ -553,7 +537,7 @@ $html= ob_start();
                                 $array [] = array('litros' =>$sum);
                             }
                         }
-                        echo json_encode($array);
+                        //echo json_encode($array);
                         
                         $resular = "";
                         foreach($arraycol as $rews)
